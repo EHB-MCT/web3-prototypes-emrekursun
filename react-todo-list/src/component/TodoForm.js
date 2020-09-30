03c340a3a509fc6715a8547cd989f9ee2bed4f24
Emre Kursun <emre.kursun@student.ehb.be>
2020-09-30 13:31:32 +0200

    Starting to understand how components work

---
+++ react-todo-list/src/component/TodoForm.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+
+function todoForm() {
+    const [todo, setTodo] = useState({
+        id:"",
+        task:"",
+        completed:"false"
+    });
+
+    <form>
+        <input />
+        <button />
+
+    </form>
+
+}
+
+export default todoForm;
